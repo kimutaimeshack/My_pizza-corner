6ccce773d5362c56b5b688a16bc8c92943a03a43
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-09-21 12:49:17 +0300

    edit on the properties

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // business logic
 
 var mybutton = document.getElementById("btn1")
-function Contact(first, last,pizzaSize,pizzaCrust,pizzatoppings) {
+function Contact(first, last, pizzaSize, pizzaCrust, pizzatoppings) {
   this.firstName = first;
   this.lastName = last;
   this.pizzaSizePrice = pizzaSize;
@@ -18,44 +18,44 @@ Contact.prototype.Totalcost = function () {
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
-
+    var pizzaCrustPrice;
+    var pizzaSizePrice ;
+    var pizzatoppingsPrice ;
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var pizzaSize = $("#PizzaSize").val();
     var pizzaCrust = $("#PizzaCrust").val();
     var pizzatoppings = $("#pizzatoppings").val();
     var pizzanumber = $("#numberofpizza").val();
-        var pizzaCrustPrice = 0;
-        var pizzaSizePrice = 0;
-        var pizzatoppingsPrice = 0;
+  
         if (pizzaSize === "700") {
-            pizzaSizePrice += 700;
+            pizzaSizePrice = ('large' + 700);
         } else if (pizzaSize === "450") {
-            pizzaSizePrice += 450;
+            pizzaSizePrice = 450;
         } else if (pizzaSize === "300") {
-            pizzaSizePrice += 300;
+            pizzaSizePrice = 300;
         }
 
         if (pizzaCrust === "400") {
-            pizzaCrustPrice += 400;
+            pizzaCrustPrice = 400;
         } else if (pizzaCrust === "160") {
-            pizzaCrustPrice += 160;
+            pizzaCrustPrice = 160;
         } else if (pizzaCrust === "200") {
-            pizzaCrustPrice += 200;
+            pizzaCrustPrice = 200;
         }
 
         if (pizzatoppings === "200") {
-            pizzatoppingsPrice += 200;
+            pizzatoppingsPrice = 200;
         } else if (pizzatoppings === "190") {
-            pizzatoppingsPrice += 190;
+            pizzatoppingsPrice = 190;
         } else if (pizzatoppings === "210") {
-            pizzatoppingsPrice += 210;
+            pizzatoppingsPrice = 210;
         }
         if (pizzatoppings === "220") {
-            pizzatoppingsPrice += 220;
+            pizzatoppingsPrice = 220;
         }
         if (pizzatoppings === "230") {
-            pizzatoppingsPrice += 230;
+            pizzatoppingsPrice = 230;
         }
 
 
@@ -64,16 +64,17 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName, pizzaSizePrice, pizzaCrustPrice, pizzatoppingsPrice);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.pizzaSizePrice + "</span></li>");
-
+   
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
 
   $("#show-contact").show();
   $("#show-contact h2").text("you order :");
   $(".first-name").text(newContact.firstName);
-  $(".last-name").text(newContact.pizzaSizePrice);
-   $("#demo").text("mee");
+  $(".last-name").text(newContact.lastName);
+  $("#demo").append(newContact.pizzaSizePrice);
+  $("#demo2").append(newContact.pizzaCrustPrice);
+  $("#demo3").append(newContact.pizzatoppingsPrice);
 
 
 
